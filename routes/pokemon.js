46f9dd2cc57115eea605628c0b628187c6ea4845
Gerardo Zamora <jgerardozc02@outlook.com>
2023-09-24 21:15:54 -0600

    Practica 8

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,33 +3,30 @@ const pokemon = express.Router();
 const db = require('../config/database');
 
 pokemon.post("/", (req, res, next) => {
-    return res.status(200).send(req.body)
+    return res.status(200).json(req.body)
 });
 
 pokemon.get("/", async(req, res, next) => {
     const pkmn = await db.query('SELECT * FROM pokemon');
-    return res.status(200).json(pkmn);
+    return res.status(200).json({code: 1, message: pkmn });
 });
 
 pokemon.get('/:id([0-9]{1,3})', async(req, res, next) => {
-    const table = await db.query('SELECT pok_id, pok_name FROM pokemon');
-    // if( id >= 0 && id <= 150) {
-        return res.status(200).json(req.body.name);
-    // }
-    // else {;
-    //     return res.status(404).send("No encontrado");
-    // }
+    const id = req.params.id;
+    if( id >= 0 && id <= 722) {
+        const pkmn = await db.query('SELECT * FROM pokemon WHERE pok_id='+id+';');
+        return res.status(200).json({code: 1, message: pkmn });
+    }
+        return res.status(404).send({code: 404, message: "Pokemon no encontrado" });
 });
 
-pokemon.get('/:name([A-Za-z]+)', (req, res, next) => {
+pokemon.get('/:name([A-Za-z]+)', async(req, res, next) => {
     const name = req.params.name;
-    const pkmn = pk.filter((p) => {
-       return (p.name.toLocaleUpperCase() == name.toUpperCase()) && p;
-        
-    });
-
-    (pkmn.length > 0) ? res.status(200).send(pkmn) : res.status(404).send("Pokemon no encontrado");
-
+    const pkmn = await db.query('SELECT * FROM pokemon WHERE pok_id='+name+';');
+    if (pkmn.length > 0) {
+        return res.status(200).json({code: 1, message: pkmn });
+    }
+    return res.status(404).send({code: 404, message: "Pokemon no encontrado" });
 });
 
 module.exports = pokemon;
