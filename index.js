927dcfb80de272b3ecf51e37edec33e4a7aa0d90
Gerardo Zamora <jgerardozc02@outlook.com>
2023-10-08 22:53:50 -0600

    Practica 13

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res, next) => {
-    return res.status(201).json({code: 1, message: "Bienvenido al Pokedex" });
+    return res.status(200).json({code: 1, message: "Bienvenido al Pokedex" });
 });
 
 app.use("/pokemon", pokemon);
 app.use("/user", user);
 
 app.use((req, res, next) => {
-    return res.status(404).json({code: 404, message: "URL no encontrada"});
+    return res.status(404).json({code: 404, message: "URL no encontrada."});
 })
 
 app.listen(process.env.PORT || 3000,() => {
