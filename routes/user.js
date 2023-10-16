d1321f3ed9d151ab13ad18d1f827ee134b0fe666
Gerardo Zamora <jgerardozc02@outlook.com>
2023-10-15 21:51:32 -0600

    change .gitignore

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const user = express.Router();
 const db = require('../config/database');
+const jwt = require('jsonwebtoken')
 
-user.post("/", async(req, res, next) => {
+user.post("/singin", async(req, res, next) => {
     const {user_name, user_mail, user_password} = req.body
 
     if(user_name && user_mail && user_password) {
@@ -18,10 +19,32 @@ user.post("/", async(req, res, next) => {
     return res.status(500).json({code: 500, message: "Campos incompletos"})
 });
 
+user.post("/login", async(req, res, next) => {
+    const {users_mail, users_password} =req.body
+    const query = `SELECT * FROM users WHERE users_mail = '${users_mail}' AND users_password = '${users_password}';`;
+    const rows = await db.query(query);
+    console.log(rows);
+
+    if(users_mail && users_password) {
+        if (rows.length == 1) {
+            const token = jwt.sign({
+                users_id: rows[0].users_id,
+                users_mail: rows[0].users_mail
+            }, 'debugkey');
+            return res.status(200).json({code: 200, message: token});
+        }
+        else {
+            return res.status(401).json({code: 401, message: "Usuario o contrasenia incorrectos"});
+        }
+    }
+    return res.status(500).json({code: 500, message: "Campos incompletos"});
+
+});
+
 user.get("/", async(req, res, next) => {
     const query = "SELECT * FROM users";
     const rows = await db.query(query);
     return res.status(200).json({code: 200, message: rows})
-})
+});
 
 module.exports = user;
